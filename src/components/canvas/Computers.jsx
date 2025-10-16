255c55790aa4eed0981e8634cf9971d6f5058c94
SaraGhabri <sarah.ghabri@polytechnicien.tn>
2025-10-16 23:02:35 +0100

    3d model error initial handling

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,37 +4,31 @@ import { Canvas } from '@react-three/fiber'
 //helpers to draw on the canvas the 3d model : useGLTF to load the model
 //  OrbitControls to move around the model, Preload to load the model before displaying it
 import{ OrbitControls, Preload, useGLTF } from '@react-three/drei'
-
 import CanvasLoader from '../Loader'
 
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
-  return (
+  const computer = useGLTF("./desktop_pc/scene.gltf");  return (
     //to create 3js element, we use mesh not div
     //to add light to mesh to see it : hemisphereLight, pointLight, spotLight
     //to add texture to the model : use useGLTF hook to load the model
     // and add it to the scene using primitive component
     // to adjust the position, rotation, scale of the model : use scale, position, rotation props
     //to make it responsive : use media query to detect screen size and adjust scale and position accordingly
-    <mesh>
-      <HemisphereLight intensity={0.15} 
-      groundColor='black' />
-      <PointLight intensity={1} />
-      <primitive
-        object={computer.scene} 
-
-      
-      />
+     <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+  
  
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-  return(
-    <Canvas 
-      frameloop='demand'
+  return (
+    <Canvas
+      frameloop="demand"
       shadows
       //camera : where are we looking at this model from 
       //fov : field of view, how much of the scene we can see
@@ -44,19 +38,19 @@ const ComputersCanvas = () => {
     >
       
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        /> 
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <Computers />
       </Suspense>
+
+
+      
       <Preload all />
-
     </Canvas>
-
-  )
-
-}
+  );
+};
 
 export default Computers
